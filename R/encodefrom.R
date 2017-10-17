6751ec9228762d7db4e98e31cce7058b8db965b1
Benjamin Skinner <btskinner@virginia.edu>
2017-10-17 09:45:19 +0000

    version 0.1.1

---
+++ R/encodefrom.R
@@ -0,0 +1,146 @@
+#' Encode data frame column using external crosswalk file.
+#'
+#' @param .data Data frame or tbl_df
+#' @param var Column name of vector to be encoded
+#' @param cw_file String of external crosswalk file, including path,
+#'     which has columns representing \code{raw} (current) vector
+#'     values, \code{clean} (new) vector values, and \code{label}s for
+#'     values. Values in \code{raw} and \code{clean} columns must be
+#'     unique (1:1 match) or an error will be thrown. Acceptable file
+#'     types include: delimited (.csv, .tsv, or other), R (.rda,
+#'     .rdata, .rds), or Stata (.dta).
+#' @param raw Name of column in \code{cw_file} that contains values in
+#'     current vector.
+#' @param clean Name of column in \code{cw_file} that contains new new
+#'     values for vector.
+#' @param label Name of column in \code{cw_file} with labels for new
+#'     values.
+#' @param delimiter String delimiter used to parse
+#'     \code{cw_file}. Only necessary if using a delimited file that
+#'     isn't a comma-separated or tab-separated file (guessed by
+#'     function based on file ending).
+#' @param sheet Specify sheet if \code{cw_file} is an Excel file and
+#'     required sheet isn't the first one.
+#' @param case_ignore Ignore case when matching current (\code{raw})
+#'     vector name with new (\code{clean}) column name.
+#' @param ignore_tibble Ignore \code{.data} status as tbl_df and
+#'     return vector as a factor rather than labelled vector.
+#' @return Vector that is either a factor or labelled, depending on
+#'     data input and options
+#' @examples
+#' df <- data.frame(state = c('Kentucky','Tennessee','Virginia'),
+#'                  stfips = c(21,47,51),
+#'                  cenregnm = c('South','South','South'))
+#'
+#' df_tbl <- tibble::as_data_frame(df)
+#'
+#' cw <- get(data(stcrosswalk))
+#'
+#' df$state2 <- encodefrom(df, state, cw, stname, stfips, stabbr)
+#' df_tbl$state2 <- encodefrom(df_tbl, state, cw, stname, stfips, stabbr)
+#' df_tbl$state3 <- encodefrom(df_tbl, state, cw, stname, stfips, stabbr,
+#'                             ignore_tibble = TRUE)
+#'
+#' haven::as_factor(df_tbl)
+#' haven::zap_labels(df_tbl)
+#' @export
+encodefrom <- function(.data,
+                       var,
+                       cw_file,
+                       raw,
+                       clean,
+                       label,
+                       delimiter = NULL,
+                       sheet = NULL,
+                       case_ignore = TRUE,
+                       ignore_tibble = FALSE
+                       ) {
+
+    ## evaluate and convert to string
+    var <- deparse(substitute(var))
+    raw <- deparse(substitute(raw))
+    clean <- deparse(substitute(clean))
+    label <- deparse(substitute(label))
+
+    ## give to _ version
+    encodefrom_(.data, var, cw_file, raw, clean, label, delimiter, sheet,
+                case_ignore, ignore_tibble)
+
+}
+
+#' @describeIn encodefrom Standard evaluation version of
+#'     \code{\link{encodefrom}} (\code{var}, \code{raw}, \code{clean},
+#'     and \code{label} must be strings when using this version)
+#'
+#' @export
+encodefrom_ <- function(.data,
+                        var,
+                        cw_file,
+                        raw,
+                        clean,
+                        label,
+                        delimiter = NULL,
+                        sheet = NULL,
+                        case_ignore = TRUE,
+                        ignore_tibble = FALSE
+                        ) {
+
+    ## read in crosswalk file if string or load if in memory
+    if (is.character(cw_file)) { cw <- get_cw_file(cw_file, delimiter, sheet) }
+    else { cw <- cw_file }
+
+    ## convert everything to character
+    .data[] <- lapply(.data, as.character)
+    cw[] <- lapply(cw, as.character)
+
+    ## confirm columns are in crosswalk
+    confirm_col(cw, raw, 'm1')
+    confirm_col(cw, clean, 'm1')
+    confirm_col(cw, label, 'm1')
+
+    ## verify that raw, clean, and label are unique in crosswalk file (1:1 mapping)
+    check_dups(cw, raw, 'm2')
+    check_dups(cw, clean, 'm2')
+    check_dups(cw, label, 'm2')
+
+    ## ignore case by setting names and var to lower
+    if (case_ignore) {
+        names(.data) <- tolower(names(.data))
+        var <- tolower(var)
+    }
+
+    ## confirm var in data
+    confirm_col(.data, var, 'm2')
+
+    ## get vector of values
+    val_vec <- .data[[var]]
+
+    ## convert raw to clean values
+    mask <- match(val_vec, cw[[raw]], nomatch = 0)
+    val_vec[mask != 0] <- cw[[clean]][mask]
+
+    if (tibble::is_tibble(.data) && !ignore_tibble) {
+
+        ## set up labels for labeller
+        val_labels <- methods::as(cw[[clean]], typeof(val_vec))
+        names(val_labels) <- cw[[label]]
+
+        ## label vector
+        val_vec_l <- labelled::labelled(val_vec, labels = val_labels)
+
+        ## return vector as labelled vector
+        return(val_vec_l)
+
+    } else {
+
+        ## get unique levels and labels (assumed in order)
+        new_lev <- unique(cw[[clean]])
+        new_lab <- cw[[label]][match(new_lev, cw[[clean]])]
+
+        ## convert to factor
+        val_vec_f <- factor(val_vec, levels = new_lev, labels = new_lab)
+
+        ## return vector as true factor
+        return(val_vec_f)
+    }
+}
---
+++ R/renamefrom.R
@@ -0,0 +1,179 @@
+#' Rename data frame columns using external crosswalk file.
+#'
+#' @param .data Data frame or tbl_df
+#' @param cw_file String of external crosswalk file, including path,
+#'     which has columns representing \code{raw} (current) column
+#'     names, \code{clean} (new) column names, and labels
+#'     (optional). Values in \code{raw} and \code{clean} columns must
+#'     be unique (1:1 match) or an error will be thrown. Acceptable
+#'     file types include: delimited (.csv, .tsv, or other), R (.rda,
+#'     .rdata, .rds), or Stata (.dta).
+#' @param raw Name of column in \code{cw_file} that contains column
+#'     names of current data frame.
+#' @param clean Name of column in \code{cw_file} that contains new
+#'     column names.
+#' @param label Name of column in \code{cw_file} with labels for
+#'     columns.
+#' @param delimiter String delimiter used to parse
+#'     \code{cw_file}. Only necessary if using a delimited file that
+#'     isn't a comma-separated or tab-separated file (guessed by
+#'     function based on file ending).
+#' @param sheet Specify sheet if \code{cw_file} is an Excel file and
+#'     required sheet isn't the first one.
+#' @param drop_extra Drop extra columns in current data frame if they
+#'     are not matched in \code{cw_file}.
+#' @param case_ignore Ignore case when matching current (\code{raw})
+#'     column names with new (\code{clean}) column names.
+#' @param keep_label Keep current label, if any, on data frame columns
+#'     that aren't matched in \code{cw_file}. Default \code{FALSE}
+#'     means that unmatched columns have any existing labels set to
+#'     \code{NULL}.
+#' @param name_label Use old (\code{raw}) column name as new
+#'     (\code{clean}) column name label. Cannot be used if
+#'     \code{label} option is set.
+#' @return Data frame or tbl_df with new column names and labels.
+#' @examples
+#' df <- data.frame(state = c('Kentucky','Tennessee','Virginia'),
+#'                  fips = c(21,47,51),
+#'                  region = c('South','South','South'))
+#'
+#' cw <- data.frame(old_name = c('state','fips'),
+#'                  new_name = c('stname','stfips'),
+#'                  label = c('Full state name', 'FIPS code'))
+#'
+#' df1 <- renamefrom(df, cw, old_name, new_name, label)
+#' df2 <- renamefrom(df, cw, old_name, new_name, name_label = TRUE)
+#' df3 <- renamefrom(df, cw, old_name, new_name, drop_extra = FALSE)
+#'
+#' @export
+renamefrom <- function(.data,
+                       cw_file,
+                       raw,
+                       clean,
+                       label = NULL,
+                       delimiter = NULL,
+                       sheet = NULL,
+                       drop_extra = TRUE,
+                       case_ignore = TRUE,
+                       keep_label = FALSE,
+                       name_label = FALSE
+                       ) {
+
+    ## evaluate and convert to string
+    raw <- deparse(substitute(raw))
+    clean <- deparse(substitute(clean))
+    label <- if (!missing(label)) { deparse(substitute(label)) }
+
+    ## give to _ version
+    renamefrom_(.data, cw_file, raw, clean, label, delimiter, sheet,
+                drop_extra, case_ignore, keep_label, name_label)
+
+}
+
+#' @describeIn renamefrom Standard evaluation version of
+#'     \code{\link{renamefrom}} (\code{raw}, \code{clean}, and
+#'     \code{label} must be strings when using this version)
+#'
+#' @export
+renamefrom_ <- function(.data,
+                        cw_file,
+                        raw,
+                        clean,
+                        label = NULL,
+                        delimiter = NULL,
+                        sheet = NULL,
+                        drop_extra = TRUE,
+                        case_ignore = TRUE,
+                        keep_label = FALSE,
+                        name_label = FALSE
+                        ) {
+
+    ## read in crosswalk file if string or load if in memory
+    if (is.character(cw_file)) { cw <- get_cw_file(cw_file, delimiter, sheet) }
+    else { cw <- cw_file }
+
+    ## convert everything to character
+    .data[] <- lapply(.data, as.character)
+    cw[] <- lapply(cw, as.character)
+
+    ## confirm columns are in crosswalk
+    confirm_col(cw, raw, 'm1')
+    confirm_col(cw, clean, 'm1')
+    if (!is.null(label)) { confirm_col(cw, label, 'm1') }
+
+    ## verify that raw and clean are unique in crosswalk file (1:1 mapping)
+    check_dups(cw, raw, 'm1')
+    check_dups(cw, clean, 'm1')
+
+    ## get starting names
+    names_ <- names(.data)
+
+    ## ignore case by setting names to lower
+    if (case_ignore) { names_ <- tolower(names_) }
+
+    ## drop unmatched names
+    if (drop_extra) {
+        .data <- .data[names_ %in% cw[[raw]]]
+
+        ## update names_
+        names_ <- names(.data)
+        if (case_ignore) { names_ <- tolower(names_) }
+    }
+
+    ## apply new names, leaving unmatched old names alone
+    mask <- match(names_, cw[[raw]], nomatch = 0)
+    new_names <- cw[[clean]][mask]
+    names(.data)[mask != 0] <- new_names
+
+    ## lists
+    label_list <- list()
+
+    ## apply new labels
+    if (!is.null(label) || name_label) {
+
+        mask <- match(names(.data), cw[[clean]], nomatch = 0)
+
+        ## get labels
+        if (!is.null(label)) {
+
+            ## labels: new from crosswalk column
+            new_labels <- cw[[label]][mask]
+
+        } else {
+
+            ## labels: old name
+            new_labels <- cw[[raw]][mask]
+
+        }
+
+        ## create list linking new names to labels
+        for (i in 1:length(new_labels)) {
+            label_list[[new_names[i]]] <- new_labels[[i]]
+        }
+
+        ## label
+        labelled::var_label(.data) <- label_list
+
+    }
+
+    ## erase old labels
+    if (!keep_label) {
+
+        extra_ <- names(.data)[!(names(.data) %in% names(label_list))]
+
+        null_label_list <- list()
+        for (i in 1:length(extra_)) {
+            null_label_list[extra_[i]] <- list(NULL)
+        }
+
+        ## set to NULL
+        labelled::var_label(.data) <- null_label_list
+
+    }
+
+    ## return data frame
+    return(.data)
+
+}
+
+
