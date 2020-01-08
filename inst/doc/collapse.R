f8d679efcc3aa08f560e454f466e5e5f383f22bd
Benjamin Skinner <btskinner@coe.ufl.edu>
2020-01-08 13:30:02 +0000

    version 0.2.6

---
+++ inst/doc/collapse.R
@@ -0,0 +1,32 @@
+## ---- message = FALSE---------------------------------------------------------
+library(crosswalkr)
+library(dplyr)
+library(haven)
+
+## -----------------------------------------------------------------------------
+## data
+df <- tibble(id = c(1:8,10,12,14:16,31,33),
+             state = c('AL','AK','AZ','AR','CA','CO','CT','DE','FL','HI',
+                       'IL','IN','IA','NJ','NY'),
+			 cendiv = c(6,9,8,7,9,8,1,5,5,9,3,3,4,2,2),
+			 cendiv_name = c('East South Central','Pacific','Mountain',
+                             'West South Central','Pacific','Mountain','New England',
+                             'South Atlantic','South Atlantic','Pacific',
+                             'East North Central','East North Central',
+                             'West North Central','Middle Atlantic','Middle Atlantic'))
+			 
+## crosswalk
+cw <- tibble(cendiv = 1:9,
+             cenreg = c(1,1,2,2,3,3,3,4,4),
+             cenregnm = c('Northeast','Northeast','Midwest','Midwest',
+                          'South','South','South','West','West'))
+
+## -----------------------------------------------------------------------------
+## encode new column
+df <- df %>%
+    mutate(cenreg = encodefrom(., var = cendiv, cw_file = cw, raw = cendiv,
+                               clean = cenreg, label = cenregnm))
+
+## -----------------------------------------------------------------------------
+df
+
--- inst/doc/crosswalkr.R
+++ inst/doc/crosswalkr.R
@@ -1,10 +1,10 @@
-## ---- message = FALSE----------------------------------------------------
+## ---- message = FALSE---------------------------------------------------------
 library(crosswalkr)
 library(dplyr)
 library(labelled)
 library(haven)
 
-## ---- echo = FALSE-------------------------------------------------------
+## ---- echo = FALSE------------------------------------------------------------
 file_1 <- data.frame(sid = c(1:3),
                      lname = c('Jackson','Harrison','Nixon'),
                      stat = c('VA','KY','IL'),
@@ -23,7 +23,7 @@ file_3 <- data.frame(s_id = c(7:9),
                      score = c(91,82,89),
                      stringsAsFactors = FALSE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 df1 <- file_1 %>%
     rename(id = sid,
            last_name = lname,
@@ -45,7 +45,7 @@ df <- rbind(df1, df2, df3)
 df
 
 
-## ---- echo = FALSE-------------------------------------------------------
+## ---- echo = FALSE------------------------------------------------------------
 crosswalk <- data.frame(clean = c('id','last_name','stabbr','score'),
                         label = c('Student ID','Student last name',
                                   'State abbreviation','Test score'),
@@ -54,7 +54,7 @@ crosswalk <- data.frame(clean = c('id','last_name','stabbr','score'),
                         file_3_raw = c('s_id','name','sta','score'),
                         stringsAsFactors = FALSE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 df1 <- renamefrom(file_1, cw_file = crosswalk, raw = file_1_raw, clean = clean, label = label)
 df2 <- renamefrom(file_2, cw_file = crosswalk, raw = file_2_raw, clean = clean, label = label)
 df3 <- renamefrom(file_3, cw_file = crosswalk, raw = file_3_raw, clean = clean, label = label)
@@ -62,29 +62,29 @@ df3 <- renamefrom(file_3, cw_file = crosswalk, raw = file_3_raw, clean = clean, 
 df <- rbind(df1, df2, df3)
 df
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 var_label(df)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 
 data(stcrosswalk)
 stcrosswalk
 
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 df1$state <- encodefrom(file_1, var = stat, stcrosswalk, raw = stabbr, clean = stfips, label = stname)
 df1
 sapply(df1, class)
 
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 file_1_ <- file_1 %>% tbl_df()
 df1$state <- encodefrom(file_1_, var = stat, stcrosswalk, raw = stabbr,
                         clean = stfips, label = stname)
 as_factor(df1)
 zap_labels(df1)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 df <- rbind(file_1 %>%
             tbl_df() %>%
             renamefrom(., crosswalk, file_1_raw, clean, label) %>%
