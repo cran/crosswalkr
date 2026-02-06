9d6c1dd2d5f5df2a2669fae1ea78b3b2f5367b4f
Benjamin Skinner <ben@btskinner.io>
2026-02-06 20:10:02 +0000

    version 0.4.0

--- inst/doc/crosswalkr.R
+++ inst/doc/crosswalkr.R
@@ -24,19 +24,19 @@ file_3 <- data.frame(s_id = c(7:9),
                      stringsAsFactors = FALSE)
 
 ## -----------------------------------------------------------------------------
-df1 <- file_1 %>%
+df1 <- file_1 |>
     rename(id = sid,
            last_name = lname,
            stabbr = stat,
            score = t_score)
 
-df2 <- file_2 %>%
+df2 <- file_2 |>
     rename(id = stu_id,
            stabbr = st,
-           score = test_score) %>%
+           score = test_score) |>
     mutate(stabbr = as.character(stabbr))
 
-df3 <- file_3 %>%
+df3 <- file_3 |>
     rename(id = s_id,
            stabbr = sta,
            last_name = name)
@@ -55,9 +55,12 @@ crosswalk <- data.frame(clean = c('id','last_name','stabbr','score'),
                         stringsAsFactors = FALSE)
 
 ## -----------------------------------------------------------------------------
-df1 <- renamefrom(file_1, cw_file = crosswalk, raw = file_1_raw, clean = clean, label = label)
-df2 <- renamefrom(file_2, cw_file = crosswalk, raw = file_2_raw, clean = clean, label = label)
-df3 <- renamefrom(file_3, cw_file = crosswalk, raw = file_3_raw, clean = clean, label = label)
+df1 <- renamefrom(file_1, cw_file = crosswalk, raw = file_1_raw, clean = clean, 
+                  label = label)
+df2 <- renamefrom(file_2, cw_file = crosswalk, raw = file_2_raw, clean = clean, 
+                  label = label)
+df3 <- renamefrom(file_3, cw_file = crosswalk, raw = file_3_raw, clean = clean, 
+                  label = label)
 
 df <- rbind(df1, df2, df3)
 df
@@ -72,35 +75,39 @@ stcrosswalk
 
 
 ## -----------------------------------------------------------------------------
-df1$state <- encodefrom(file_1, var = stat, stcrosswalk, raw = stabbr, clean = stfips, label = stname)
+df1$state <- encodefrom(file_1, var = stat, stcrosswalk, raw = stabbr, 
+                        clean = stfips, label = stname)
 df1
 sapply(df1, class)
 
 
 ## -----------------------------------------------------------------------------
-file_1_ <- file_1 %>% tbl_df()
+file_1_ <- file_1 |> tibble::as_tibble()
 df1$state <- encodefrom(file_1_, var = stat, stcrosswalk, raw = stabbr,
                         clean = stfips, label = stname)
 as_factor(df1)
 zap_labels(df1)
 
 ## -----------------------------------------------------------------------------
-df <- rbind(file_1 %>%
-            tbl_df() %>%
-            renamefrom(., crosswalk, file_1_raw, clean, label) %>%
-            mutate(stabbr = encodefrom(., stabbr, stcrosswalk, stabbr, stfips, stname)),
+df <- rbind(file_1 |>
+            tibble::as_tibble() |>
+            renamefrom(crosswalk, file_1_raw, clean, label)  %>%
+            mutate(stabbr = encodefrom(., stabbr, stcrosswalk, stabbr, stfips, 
+                                        stname)),
 
             ## append file 2
-            file_2 %>%
-            tbl_df() %>%
-            renamefrom(., crosswalk, file_2_raw, clean, label) %>%
-            mutate(stabbr = encodefrom(., stabbr, stcrosswalk, stfips, stfips, stname)),
+            file_2 |>
+            tibble::as_tibble() |>
+            renamefrom(crosswalk, file_2_raw, clean, label) %>%
+            mutate(stabbr = encodefrom(., stabbr, stcrosswalk, stfips, stfips, 
+                                        stname)),
 
             ## append file 3
-            file_3 %>%
-            tbl_df() %>%
-            renamefrom(., crosswalk, file_3_raw, clean, label) %>%
-            mutate(stabbr = encodefrom(., stabbr, stcrosswalk, stname, stfips, stname)))
+            file_3 |>
+            tibble::as_tibble() |>
+            renamefrom(crosswalk, file_3_raw, clean, label) %>%
+            mutate(stabbr = encodefrom(., stabbr, stcrosswalk, stname, stfips, 
+                                        stname)))
 
 df
 as_factor(df)            
